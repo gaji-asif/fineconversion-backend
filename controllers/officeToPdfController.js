9e576cfbadab8f6621c4b2deee16b61d1f9fc401
Abdullah Al Mamun <abdullahalmamun1804064@gmail.com>
2023-06-21 18:50:28 +0600

    office -to -pdf done

--- controllers/officeToPdfController.js
+++ controllers/officeToPdfController.js
@@ -1,6 +1,9 @@
 const catchAsyncError = require("../middlewares/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 const path = require("path");
+const fs = require("fs");
+const libre = require("libreoffice-convert");
+libre.convertAsync = require("util").promisify(libre.convert);
 
 exports.convertToPdf = catchAsyncError(async (req, res, next) => {
   let fileExt = path.extname(req.file.originalname).toLocaleLowerCase();
@@ -9,9 +12,39 @@ exports.convertToPdf = catchAsyncError(async (req, res, next) => {
 
   if (fileExt !== req.body.type) {
     return next(
-      new ErrorHandler(`The ${req.body.type} file is only allowed `, 405),
+      new ErrorHandler(`This ${req.body.type} file is not allowed.`, 405),
     );
   }
 
-  res.status(200).json({ message: "success" });
+  fileExt = path.extname(req.file.originalname);
+  const docxBuf = await fs.promises.readFile(req.file.path);
+  let outputPath = path.join(
+    __dirname,
+    "..",
+    "public/uploads",
+    `${req.file.originalname.replace(fileExt,"").split(" ")
+      .join("-") + '-' + Date.now()}.pdf`,
+  );
+
+  try {
+    const pdfBuf = await libre.convertAsync(docxBuf, ".pdf", undefined);
+
+    await fs.promises.writeFile(outputPath, pdfBuf);
+
+    res.download(outputPath, (err) => {
+      if (err) {
+        next(new Error("Error occurred during the download process.", 404));
+      }
+
+      fs.promises.unlink(req.file.path);
+      fs.promises.unlink(outputPath);
+    });
+  } catch (err) {
+    fs.promises.unlink(req.file.path);
+    fs.promises.unlink(outputPath);
+
+    next(
+      new ErrorHandler("Error occurred during the conversion process.", 404),
+    );
+  }
 });
